f689b3872ae9f737e152c27c59dff754fbc4d948
bdubbs <bdubbs@4aa44e1e-78dd-0310-a6d2-fbcd4c07a689>
2009-11-24 18:22:00 +0000

    Fix pdf overflow of grub listing output by changing the font size in the appropriate block of the lfs-pdf.fo file.

---
+++ pdf-fixups.sh
@@ -0,0 +1,15 @@
+#!/bin/bash --verbose
+
+if [ $# -lt 1 ] ; then 
+  echo "This script needs the location of the fo file to update"
+  exit 1
+fi
+
+
+FILE=$1
+
+LINE=$( grep -n "DO NOT EDIT" $FILE | cut -f1 -d: )
+LINE=$(( LINE - 1 ))
+
+sed -i -e "$LINE s/monospace/&\" font-size=\"9pt/" $FILE
+
