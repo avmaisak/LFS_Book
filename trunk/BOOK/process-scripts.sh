b3b817be24a4a5526b94548a74499c978a42f554
bdubbs <bdubbs@4aa44e1e-78dd-0310-a6d2-fbcd4c07a689>
2012-01-16 06:44:15 +0000

    Show sysconfig/udev_retry in Appendix D

--- trunk/BOOK/process-scripts.sh
+++ trunk/BOOK/process-scripts.sh
@@ -13,8 +13,9 @@ do
   #[ $script == 'services'        ] && continue
 
   # Disambiguate duplicate file names
-  [ $s == 'bootscripts/lfs/sysconfig/rc'      ] && script='rc-sysinit'; 
-  [ $s == 'bootscripts/lfs/sysconfig/modules' ] && script='modules-sysinit'; 
+  [ $s == 'bootscripts/lfs/sysconfig/rc'         ] && script='rc-sysinit'; 
+  [ $s == 'bootscripts/lfs/sysconfig/modules'    ] && script='modules-sysinit'; 
+  [ $s == 'bootscripts/lfs/sysconfig/udev_retry' ] && script='config-udev-retry'; 
   
   sed  -e 's/\&/\&amp\;/g' -e 's/</\&lt\;/g'   -e 's/>/\&gt\;/g' \
        -e "s/'/\&apos\;/g" -e 's/"/\&quot\;/g' -e 's/\t/    /g'  \
