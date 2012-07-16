328258dffd48f2214baf40f88a98ccac0a7a72f8
bdubbs <bdubbs@4aa44e1e-78dd-0310-a6d2-fbcd4c07a689>
2012-07-16 17:19:20 +0000

    Updated udev to version 186.  This update has a major procedure change due to merging systemd and udev.

--- make-aux-files.sh
+++ make-aux-files.sh
@@ -10,13 +10,13 @@ mv bootscripts lfs-bootscripts-$version
 tar -cjf lfs-bootscripts-$version.tar.bz2 --exclude .svn lfs-bootscripts-$version
 mv lfs-bootscripts-$version bootscripts 
 
-rm -f udev-config*.bz2
+#rm -f udev-config*.bz2
 
 # Get file name and move udev config directory to that name
-version=`grep "ENTITY udev-config " packages.ent |cut -d'"' -f2`
-mv udev-config $version
+#version=`grep "ENTITY udev-config " packages.ent |cut -d'"' -f2`
+#mv udev-config $version
 
 # Create the tarball and clean up
-tar -cjf $version.tar.bz2 --exclude .svn $version
-mv $version udev-config
+#tar -cjf $version.tar.bz2 --exclude .svn $version
+#mv $version udev-config
 
