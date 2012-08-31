52c9e39ea0e4ceeae16d87556cd92a45fd1ef63e
bdubbs <bdubbs@4aa44e1e-78dd-0310-a6d2-fbcd4c07a689>
2012-08-31 18:32:57 +0000

    Clean up build rules Delete obsolete sysconfig/rc file

--- process-scripts.sh
+++ process-scripts.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Boot scripts
-for s in bootscripts/lfs/init.d/*                    \
-         bootscripts/lfs/sysconfig/*                 \
+for s in bootscripts/lfs/init.d/*       \
+         bootscripts/lfs/sysconfig/*    \
          bootscripts/lfs/lib/services/* \
-         bootscripts/lfs/sbin/*
+         bootscripts/lfs/sbin/*         \
+         ../udev-lfs/*.rules
 do
   script=$(basename $s)
   
@@ -17,16 +18,6 @@ do
   [ $s == 'bootscripts/lfs/sysconfig/modules'    ] && script='modules-sysinit'; 
   [ $s == 'bootscripts/lfs/sysconfig/udev_retry' ] && script='config-udev-retry'; 
   
-  sed  -e 's/\&/\&amp\;/g' -e 's/</\&lt\;/g'   -e 's/>/\&gt\;/g' \
-       -e "s/'/\&apos\;/g" -e 's/"/\&quot\;/g' -e 's/\t/    /g'  \
-       $s > appendices/${script}.script 
-done
-
-# Udev rules
-for s in udev-config/*.rules
-do
-  script=$(basename $s)
-
   sed  -e 's/\&/\&amp\;/g' -e 's/</\&lt\;/g'   -e 's/>/\&gt\;/g' \
        -e "s/'/\&apos\;/g" -e 's/"/\&quot\;/g' -e 's/\t/    /g'  \
        $s > appendices/${script}.script 
