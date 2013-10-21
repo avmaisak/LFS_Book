9aee33f9af3545a48744c40a4ba9a21e645d4ff9
matthew <matthew@4aa44e1e-78dd-0310-a6d2-fbcd4c07a689>
2013-10-21 22:08:54 +0000

    Fix a few more typos and whitespaces.

--- aux-file-data.sh
+++ aux-file-data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $# -lt 1 ] ; then 
+if [ $# -lt 1 ] ; then
   echo "This script needs the location of the xml file to update"
   exit 1
 fi
@@ -19,7 +19,7 @@ bootmd5=$(md5sum $bootscripts | cut -f1 -d" ")
 TOPDIR=$(pwd)
 TMP_DIR=$(mktemp -d /tmp/lfsbootfiles.XXXXXX)
 pushd $TMP_DIR > /dev/null
-tar -xf $TOPDIR/$bootscripts 
+tar -xf $TOPDIR/$bootscripts
 bootinstallsize=$(du -sk $TMP_DIR | cut -f1)
 popd > /dev/null
 rm -rf $TMP_DIR
