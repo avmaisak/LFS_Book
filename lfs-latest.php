274a198e807188e820ad37379f1a2d7e9c5fba33
Anton Maisak <am@rus-bit.com>
2018-06-15 22:22:31 +0300

    SYNC
    Revision: 11420
    Author: bdubbs
    Date: 15 июня 2018 г. 19:22:09
    Message:
    Update currency script for elfutils
    ----
    Modified : /trunk/BOOK/chapter07/udev.xml
    Modified : /trunk/BOOK/chapter07/usage.xml
    Modified : /trunk/BOOK/lfs-latest.php

--- lfs-latest.php
+++ lfs-latest.php
@@ -29,13 +29,13 @@ function find_max( $lines, $regex_match, $regex_replace )
 
   foreach ( $lines as $line )
   {
-     if ( ! preg_match( $regex_match, $line ) ) continue; 
+     if ( ! preg_match( $regex_match, $line ) ) continue;
 
      // Isolate the version and put in an array
      $slice = preg_replace( $regex_replace, "$1", $line );
-     if ( $slice == $line ) continue; 
+     if ( $slice == $line ) continue;
 
-     array_push( $a, $slice );     
+     array_push( $a, $slice );
   }
 
   // SORT_NATURAL requires php-5.4.0 or later
@@ -96,7 +96,7 @@ function max_parent( $dirpath, $prefix )
   $regex_replace = "#^.*(${prefix}[\d\.]+)/.*$#";
   $max           = find_max( $lines, $regex_match, $regex_replace );
 
-  return "$dirpath/$max"; 
+  return "$dirpath/$max";
 }
 
 function get_packages( $package, $dirpath )
@@ -109,7 +109,8 @@ function get_packages( $package, $dirpath )
 if ( $package == "check"      ) $dirpath = "https://github.com/libcheck/check/releases";
 if ( $package == "e2fsprogs"  ) $dirpath = "http://sourceforge.net/projects/e2fsprogs/files/e2fsprogs";
 if ( $package == "expat"      ) $dirpath = "http://sourceforge.net/projects/expat/files";
-if ( $package == "expect"     ) $dirpath = "http://sourceforge.net/projects/expect/files";  
+if ( $package == "elfutils"   ) $dirpath = "https://sourceware.org/ftp/elfutils";
+if ( $package == "expect"     ) $dirpath = "http://sourceforge.net/projects/expect/files";
 if ( $package == "file"       ) $dirpath = "https://github.com/file/file/releases";
 if ( $package == "flex"       ) $dirpath = "https://github.com/westes/flex/releases";
 if ( $package == "gcc"        ) $dirpath = max_parent( $dirpath, "gcc-" );
@@ -127,8 +128,8 @@ if ( $package == "util-linux" ) $dirpath = max_parent( $dirpath, "v." );
 if ( $package == "vim"        ) $dirpath = "ftp://ftp.vim.org/pub/vim/unix";
 
   // Check for ftp
-  if ( preg_match( "/^ftp/", $dirpath ) ) 
-  { 
+  if ( preg_match( "/^ftp/", $dirpath ) )
+  {
     $dirpath  = substr( $dirpath, 6 );           // Remove ftp://
     $dirpath  = rtrim ( $dirpath, "/" );         // Trim any trailing slash
     $position = strpos( $dirpath, "/" );         // Divide at first slash
@@ -136,7 +137,7 @@ if ( $package == "vim"        ) $dirpath = "ftp://ftp.vim.org/pub/vim/unix";
     $path     = substr( $dirpath, $position );
 
     $conn = ftp_connect( $server );
-    ftp_login( $conn, "anonymous", "" ); 
+    ftp_login( $conn, "anonymous", "" );
 
     // See if we need special handling
     if ( isset( $exceptions[ $package ] ) )
@@ -155,7 +156,7 @@ if ( $package == "vim"        ) $dirpath = "ftp://ftp.vim.org/pub/vim/unix";
               $path = substr( $path, 0, $position );
 
               // Get dir listing
-              $lines = ftp_rawlist ($conn, $path);              
+              $lines = ftp_rawlist ($conn, $path);
               $max   = find_max( $lines, $regexp, $regexp );
               break;
 
@@ -188,7 +189,7 @@ if ( $package == "vim"        ) $dirpath = "ftp://ftp.vim.org/pub/vim/unix";
         $dirpath  = substr ( $dirpath, 0, $position );
      }
 
-     if ( $package == "bzip2" ) 
+     if ( $package == "bzip2" )
      {
         // Remove one directory
         $dirpath  = rtrim  ( $dirpath, "/" );    // Trim any trailing slash
@@ -230,7 +231,7 @@ if ( $package == "vim"        ) $dirpath = "ftp://ftp.vim.org/pub/vim/unix";
      $lines = $tmp;
   }
 
-  if ( $package == "attr" ||  
+  if ( $package == "attr" ||
        $package == "acl"  )
   {
      return find_max( $lines, "/$package/", "/^.*$package-([\d\.-]*\d)\.src.*$/" );
@@ -241,6 +242,9 @@ if ( $package == "vim"        ) $dirpath = "ftp://ftp.vim.org/pub/vim/unix";
 
   if ( $package == "expect" )
      return find_max( $lines, "/expect/", "/^.*expect(\d[\d\.]+\d).tar.*$/" );
+
+  if ( $package == "elfutils" )
+     return find_max( $lines, "/^\d/", "/^(\d[\d\.]+\d)\/.*$/" );
 
   if ( $package == "XML-Parser" )
   {
@@ -333,8 +337,8 @@ function get_current()
         $pattern = "/\D*(\d.*[a-z]*)\.tar\D*/";
       }
 
-      else if ( preg_match( "/systemd-man-pages/", $file ) ) continue; 
-      else if ( preg_match( "/python/"         , $file ) ) continue; 
+      else if ( preg_match( "/systemd-man-pages/", $file ) ) continue;
+      else if ( preg_match( "/python/"         , $file ) ) continue;
 
       $version = preg_replace( $pattern, "$1", $file );   // Isolate version
       $version = preg_replace( "/^\d-/", "", $version );  // Remove leading #-
